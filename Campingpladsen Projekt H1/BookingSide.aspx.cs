214cddfc6e5512f83d343460887b6a1f5248e1c1
LinusTD <9331149-LinusTD@users.noreply.gitlab.com>
2021-10-13 10:38:51 +0200

    clean up

--- Campingpladsen Projekt H1/BookingSide.aspx.cs
+++ Campingpladsen Projekt H1/BookingSide.aspx.cs
@@ -12,11 +12,6 @@ namespace Campingpladsen_Projekt_H1
 {
     public partial class BookingSide : System.Web.UI.Page
     {
-        // Insert numbers for dropdown lists
-        For i As Integer = 1 To 15
-            dropDownListVoksne.Items.Add(new ListItem(i.ToString(), i.ToString()))
-        Next i
-
         // Generates random number for reservation
         Random r = new Random();
         protected void Page_Load(object sender, EventArgs e) {}
